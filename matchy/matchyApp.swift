5af9e2ebe0437c8abd685dbe26b26cb44b329831
Rafa <rafavazquez229@gmail.com>
2021-01-06 20:47:49 -0800

    Initial Commit

---
+++ matchy/matchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchyApp.swift
+//  matchy
+//
+//  Created by Rafael Vazquez on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct matchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
